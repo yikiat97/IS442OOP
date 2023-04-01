4bc517f2e1b7638446139d548a60d18158b21092
chonghui246 <106052137+chonghui246@users.noreply.github.com>
2023-04-01 14:43:55 +0800

    change approver table style to quantum table style

--- frontend/src/pages/Approver.js
+++ frontend/src/pages/Approver.js
@@ -33,6 +33,7 @@ import {
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
+import QuantumDetailTable from "../components/QuantumDetailTable";
 
 
 function Approver(){
@@ -93,52 +94,24 @@ function Approver(){
     };
 
     return(
-        <Grid sx={{mt:6, textAlign:'left', px:4}}>
-            
-            <Grid container spacing={{ md: 6 }} columns={{xs:12, sm:4,md:4}} sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
-                <Grid item md={2.0} sm={2.5}>
-                    <h1>User Management</h1>
-                </Grid>
+      <Grid sx={{mt:6, textAlign:'left', px:4}}>
+              
+        <Grid container spacing={{ md: 6 }} columns={{xs:12, sm:4,md:4}} sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
+            <Grid item md={2.0} sm={2.5}>
+                <h1>User Management</h1>
+            </Grid>
+        </Grid>
+
+        <Paper elevation={1} sx={{height:"100%", pt:1,pl:2,pb:2, my:3}} md={{}}>
+            <Grid sx={{mx:2, mb:4}} columns={{ xs: 12, sm: 12, md: 12 }}>
+                <h2>{companyDetails.name} Approvers</h2>
             </Grid>
 
-            <Paper elevation={1} sx={{height:"100%", pt:1,pl:2,pb:2, my:3}} md={{}}>
-                <Grid sx={{mx:2, mb:4}} columns={{ xs: 12, sm: 12, md: 12 }}>
-                    <h2>{companyDetails.name} Approvers</h2>
-                </Grid>
-
-                <Grid sx={{mx:2, mb:4}} columns={{ xs: 12, sm: 12, md: 12 }}>
-                   <Table size="small" aria-label="contacts">
-                        <TableHead>
-                        <StyledTableRow>
-                            <StyledTableCell align="left">Contact Name</StyledTableCell>
-                            <StyledTableCell align="left">Email</StyledTableCell>
-                            <StyledTableCell align="left">Contact Number</StyledTableCell>
-                            <StyledTableCell align="left">User Role</StyledTableCell>
-                            <StyledTableCell align="right"></StyledTableCell>
-                            <StyledTableCell align="right"></StyledTableCell>
-                        </StyledTableRow>
-                        </TableHead>
-                        <TableBody>
-                            {users
-                                .map((user) => (
-                                    <StyledTableRow key={user.name}>
-                                        <StyledTableCell component="th" scope="row">
-                                            {user.name}
-                                        </StyledTableCell>
-                                        <StyledTableCell align="left">{user.email}</StyledTableCell>
-                                        <StyledTableCell align="left">{user.contactNumber == null ? "Not available" : user.contactNumber}</StyledTableCell>
-                                        <StyledTableCell align="left">{user.role}</StyledTableCell>
-                                        <StyledTableCell align="right"><DeleteOutlineIcon sx={{color:'#c62828'}}/></StyledTableCell>
-                                        <Link href={'../EditUser/' + companyDetails.registrationNum + "/" + user.email} underline='none'>
-                                            <StyledTableCell align="left"><EditIcon sx={{color:'#1565c0'}} /></StyledTableCell>
-                                        </Link>
-                                    </StyledTableRow>
-                            ))}
-                        </TableBody>
-                    </Table> 
-                </Grid>
-            </Paper>
-        </Grid>
+            <Grid sx={{ mx: 2, mb: 4 }} columns={{ xs: 12, sm: 12, md: 12 }}>
+                <QuantumDetailTable props={{ users, companyDetails }} />
+            </Grid>
+        </Paper>
+      </Grid>
         
     )
     
